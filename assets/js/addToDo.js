f9b153f8b41e9bd27bef2811c1ac8558fe8e6b4e
Luciano <luciano.cia3@gmail.com>
2023-04-26 17:26:12 +0200

    "réglade du bug d'inx pendant l'ajout"

--- assets/js/addToDo.js
+++ assets/js/addToDo.js
@@ -2,7 +2,7 @@ export function addToDo(myListToDo) {
   let myToDo = myListToDo;
   document.querySelector("form").addEventListener("submit", (e) => {
     // e.preventDefault();
-    let n = myListToDo.length;
+    let n = localStorage.getItem("index");
     if (document.getElementById("titre").value != "") {
       myToDo.push({
         index: ++n,
@@ -15,6 +15,7 @@ export function addToDo(myListToDo) {
       localStorage.clear();
       myToDo.forEach((e) => {
         window.localStorage.setItem(e.index, JSON.stringify(e));
+        window.localStorage.setItem("index", n);
       });
     }
   });
--- assets/js/delToDo.js
+++ assets/js/delToDo.js
@@ -4,9 +4,11 @@ export function delToDo(listToDo) {
       listToDo.forEach((myToDo) => {
         if (myToDo.title == e.target.parentElement.firstChild.textContent) {
           listToDo.splice(listToDo.indexOf(myToDo), 1);
+          let n = localStorage.getItem("index");
           localStorage.clear();
           listToDo.forEach((e) => {
             window.localStorage.setItem(e.index, JSON.stringify(e));
+            window.localStorage.setItem("index", n);
           });
         }
         location.reload();
